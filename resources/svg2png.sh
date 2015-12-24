766532ea88662df996850240d190a9378b80eb0c
jcome <jcome@localhost.localdomain>
2015-12-24 13:04:41 +0800

    Update the path

--- resources/svg2png.sh
+++ resources/svg2png.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-for file in *.svg; do inkscape $file -w 24 -h 24 -e ../../output/png-24px/glyphs/${file%svg}png; done
+for file in *.svg; do inkscape $file -w 24 -h 24 -e ../../output/glyphs/${file%svg}png; done
